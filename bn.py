6efa2fd3bd001de3a8071e4290dfeb082e3d0b0f
iku <31121811+ikbal-nayem@users.noreply.github.com>
2018-07-22 18:07:56 +0600

    Add files via upload

---
+++ bn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+old_dir = os.getcwd()
+os.chdir('/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-LVDS-1/intel_backlight')
+
+while True:
+	try:
+		with open("brightness", 'r') as f:
+			old_value = f.read()
+			print('Current brightness is {}'.format(old_value))
+		f.close()
+
+		value = int(input('Put your new value here : '))
+		if value < 50 or value > 1000:
+			f.close()
+			print('\n\tSetting done!!!')
+			break
+		with open('brightness', 'w') as f:
+			f.write(str(value))
+		f.close()
+	except:
+		print('\n\tSetting done!!!')
+		break
+
+os.chdir(old_dir)
