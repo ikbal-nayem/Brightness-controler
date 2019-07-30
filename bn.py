d18a0d75e16c5822deb47fcbefd0861f7d7811f6
iku <31121811+ikbal-nayem@users.noreply.github.com>
2019-07-30 22:51:25 +0600

    added keyboard shortcut

--- bn.py
+++ bn.py
@@ -1,23 +1,51 @@
 #!/usr/bin/env python3
 import os
-old_dir = os.getcwd()
-os.chdir('/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-LVDS-1/intel_backlight')
+import datetime, time
+import keyboard
 
-while True:
-	try:
-		with open("brightness", 'r') as f:
-			old_value = f.read()
-			print('Current brightness is {}'.format(old_value))
+brightness_dir = '/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-LVDS-1/intel_backlight'
 
-		value = int(input('Put your new value here : '))
-		if value < 50 or value > 1000:
-			f.close()
-			print('\n\tSetting done!!!')
-			break
-		with open('brightness', 'w') as f:
-			f.write(str(value))
-	except:
-		print('\n\tSetting done!!!')
-		break
+class Brightness:
 
-os.chdir(old_dir)
+	def change(self, light):
+		try:
+		    if light > 50 and light < 1000:
+		        with open(os.path.join(brightness_dir, 'brightness'), 'w') as f:
+		            f.write(str(light))
+		        with open('last_brightness', 'w') as f:
+		            f.write(str(light))
+		except:
+			print('Error found!')
+
+	def up(self):
+		with open('last_brightness', 'r') as f:
+			light = int(f.read())
+			light += 100
+			self.change(light)
+
+	def down(self):
+		with open('last_brightness', 'r') as f:
+			light = int(f.read())
+			light -= 100
+			self.change(light)
+
+	def startUp(self):
+		light = ''
+		try:
+		    with open('last_brightness', 'r') as f:
+		        light = int(f.read())
+		except:
+		    with open('last_brightness', 'w') as f:
+		        light = 800
+		        f.write(str(light))
+		self.change(light)
+
+
+if __name__=='__main__':
+	control = Brightness()
+	control.startUp()
+	keyboard.add_hotkey('ctrl+right', control.up, suppress=False, timeout=1, trigger_on_release=False)
+	keyboard.add_hotkey('ctrl+left', control.down, suppress=False, timeout=1, trigger_on_release=False)
+
+	while True:
+		time.sleep(0.1)
